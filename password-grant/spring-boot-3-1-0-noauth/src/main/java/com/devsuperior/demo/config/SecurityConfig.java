0e0c097feff9dd3fe226544c5a6cf57021adcf38
andradeGabriel05 <gabriel.andrade@sptech.school>
2024-12-12 21:10:52 -0300

    spring security & security config

---
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()); // desabilita proteção contra ataques csrf
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // permissão para endpoints
+        return http.build();
+    }
+}
